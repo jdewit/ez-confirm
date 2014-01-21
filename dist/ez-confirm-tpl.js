e7d467ba8984eb4c6f1042195ad6bdec64de9ea2
Joris de Wit <joris.w.dewit@gmail.com>
2014-01-20 21:20:04 -0800

    fix modal body class

--- dist/ez-confirm-tpl.js
+++ dist/ez-confirm-tpl.js
@@ -7,7 +7,7 @@ angular.module('ez.confirm').run(['$templateCache', function($templateCache) {
     "    <a type=\"button\" class=\"close\" ng-click=\"cancel()\" aria-hidden=\"true\">&times;</a>\n" +
     "    <h3>{{ heading }}</h3>\n" +
     "  </div>\n" +
-    "  <div class=\"modal-content\">\n" +
+    "  <div class=\"modal-body\">\n" +
     "    <p class=\"lead\">{{ text }}</p>\n" +
     "  </div>\n" +
     "  <div class=\"modal-footer\">\n" +
