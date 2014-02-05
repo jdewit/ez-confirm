62bba7798f98ef988280c490cdc4c6478dac225d
Joris de Wit <joris.w.dewit@gmail.com>
2014-02-05 10:40:58 -0800

    update to bs2 & fa4

--- dist/ez-confirm-tpl.js
+++ dist/ez-confirm-tpl.js
@@ -2,7 +2,7 @@ angular.module('ez.confirm').run(['$templateCache', function($templateCache) {
   'use strict';
 
   $templateCache.put('ez-confirm-tpl.html',
-    "<div class=\"ez-confirm modal\">\n" +
+    "<div class=\"ez-confirm\">\n" +
     "  <div class=\"modal-header\">\n" +
     "    <a type=\"button\" class=\"close\" ng-click=\"cancel()\" aria-hidden=\"true\">&times;</a>\n" +
     "    <h3>{{ heading }}</h3>\n" +
@@ -11,8 +11,8 @@ angular.module('ez.confirm').run(['$templateCache', function($templateCache) {
     "    <p class=\"lead\">{{ text }}</p>\n" +
     "  </div>\n" +
     "  <div class=\"modal-footer\">\n" +
-    "    <a class=\"btn\" ng-click=\"cancel()\"><i class=\"icon-remove\"></i>Cancel</a>\n" +
-    "    <button ng-click=\"ok()\" class=\"btn btn-large btn-primary\"><i class=\"icon-ok\"></i>Yes</button>\n" +
+    "    <a class=\"btn btn-default\" ng-click=\"cancel()\"><i class=\"fa fa-times\"></i>Cancel</a>\n" +
+    "    <button ng-click=\"ok()\" class=\"btn btn-lg btn-primary\"><i class=\"fa fa-check\"></i>Yes</button>\n" +
     "  </div>\n" +
     "</div>\n"
   );
